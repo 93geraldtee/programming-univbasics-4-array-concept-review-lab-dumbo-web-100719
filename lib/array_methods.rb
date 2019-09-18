7f441e9045b62de51fd4a411d55b050ddc608882
Gerald Tee <93geraldtee@gmail.com>
2019-09-18 19:54:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,7 +9,7 @@ end
 
 
 def find_max_value(array)
-  x = 0
+  x = array[0]
   array.length.times do |index|
     if array[index] > x
       x = array[index]
@@ -20,7 +20,7 @@ end
 
 
 def find_min_value(array)
-  x = 0
+  x = array[0]
   array.length.times do |index|
     if array[index] < x
       x = array[index]
